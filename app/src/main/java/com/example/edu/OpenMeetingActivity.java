bd0e5f6a0e08efcabc523531f1c795fc5d9ce832
jang <jbh332@naver.com>
2018-09-02 12:10:30 +0900

    [BH]OPENMEETING
    XML, JAVA 수정 / border,xml 추가 ( 네모칸 만드는 스타일?) / array.xml 추가 ( 스피너 아이템 목록 )

--- app/src/main/java/com/example/edu/OpenMeetingActivity.java
+++ app/src/main/java/com/example/edu/OpenMeetingActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.edu.databinding.ActivityOpenMeetingBinding;
 
 public class OpenMeetingActivity extends AppCompatActivity {
 
     ActivityOpenMeetingBinding b;
+ArrayAdapter adapter;
+Spinner spinner;
 
 
     @Override
@@ -22,6 +26,10 @@ public class OpenMeetingActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        adapter = ArrayAdapter.createFromResource(this, R.array.topic, android.R.layout.simple_dropdown_item_1line);
+        spinner = (Spinner) findViewById(R.id.topicSpinner);
+        spinner.setAdapter(adapter);
 
 
 
