15b2a65820e7a65a5c4af6bc13faea4f59120775
jaegyun <jk6370@gmail.com>
2018-09-08 11:27:51 +0900

    [jaegyun] firebase 2-8 linkage

--- app/src/main/java/com/example/edu/RecyclerAdpater.java
+++ app/src/main/java/com/example/edu/RecyclerAdpater.java
@@ -1,12 +1,17 @@
 package com.example.edu;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.edu.chat.MessageActivity;
 import com.example.edu.model.BoardModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -42,14 +47,30 @@ public class RecyclerAdpater extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) { // 뷰홀더가 만들어지는 시점에 호출되는 메소드
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
         // parent : 각각의 아이템을 위해서 정의한 xml 레이아웃의 최상위 레이아웃 , 어떤 것 (item.xml)으로 뷰를 만들고 그것을 뷰홀더에 넣어줄지 결정.
         return new CustomViewHolder(view); // >> 각각의 아이템을 위한 뷰를 담고 있는 뷰홀더 객체를 만들어서 리턴
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) { // 데이터와 뷰가 서로 결합되는 경우
-        ((CustomViewHolder)holder).tv.setText(boardModels.get(position).groupName);
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) { // 데이터와 뷰가 서로 결합되는 경우
+        ((CustomViewHolder) holder).tv.setText(boardModels.get(position).groupName);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MessageActivity.class);
+                intent.putExtra("destinationUid", boardModels.get(position).uid);
+                Log.e("kkkk", boardModels.get(position).uid); //test중입니다
+                Log.e("kkkk", "test"); //test중입니다
+
+                ActivityOptions activityOptions = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    activityOptions = ActivityOptions.makeCustomAnimation(view.getContext(), R.anim.fromright, R.anim.toleft);
+                    view.getContext().startActivity(intent, activityOptions.toBundle());
+                }
+            }
+        });
     }
 
     @Override
@@ -61,6 +82,7 @@ public class RecyclerAdpater extends RecyclerView.Adapter<RecyclerView.ViewHolde
         ImageView iv;
         TextView tv, tv2, tv3;
         ShineButton shineButton;
+
         public CustomViewHolder(View view) { // 뷰홀더 생성자 , 뷰홀더는 각각의 아이템을 위한 뷰를 담고 있다
             super(view); // 그러므로 뷰홀더는 Items.xml 의 뷰를 전달해주는 것, Items의 컨텐츠를 이용해 데이터 설정등등 진행
             // 뷰와 실제 데이터 ( java 내의 할당된 데이터 ) 매칭 과정
